eb2df36c39802e0db7dcf9ade48bf8ec477f8e58
Henrik Meinardus <holy-clive@gmx.de>
2016-09-02 16:11:07 +0000

    Implement basic react components and navigation

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>About</div>
+    )
+  }
+}
+
+export default AboutPage
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import routes from './routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+render((
+  <Router history={browserHistory} routes={routes} />
+), document.getElementById('app'))
